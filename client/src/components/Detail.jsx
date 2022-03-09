cebebdb93e07965ac0240d5ee1309625299759f1
Dylan Juresa Furlong <dylanjuresa@gmail.com>
2022-03-08 21:48:10 -0300

    DetailDog Responsive rotacion corregida

--- client/src/components/Detail.jsx
+++ client/src/components/Detail.jsx
@@ -25,12 +25,16 @@ export default function Detail() {
             {dogInfo.length !== 0 ?
                 <div className={s.estiloContainer}>
                     <img src={dogInfo[0].img} alt="A" width="200px" height="250px" />
-                    <h4>Nombre: {dogInfo[0].name}</h4>
-                    <h5>Peso: {`${dogInfo[0].weight} Kgs`}</h5>
-                    <h5>Altura: {`${dogInfo[0].height} cm`}</h5>
-                    <h5>Temperamentos: {dogInfo[0].temperamentos.toString()}</h5>
-                    <h5>Años de vida:  {`${dogInfo[0].yearsLife} `}</h5>
-                    <Link to="/dogs">Volver a Home</Link>
+                    <div className={s.info}>
+                        <h5>Nombre: {dogInfo[0].name}</h5>
+                        <h5>Peso: {`${dogInfo[0].weight} Kgs`}</h5>
+                        <h5>Altura: {`${dogInfo[0].height} cm`}</h5>
+                        <h5>Temperamentos: {dogInfo[0].temperamentos.toString()}</h5>
+                        <h5>Años de vida:  {`${dogInfo[0].yearsLife} `}</h5>
+                        <Link to="/dogs">Volver a Home</Link>
+                    </div>
+
+                  
 
                 </div> : false}
 
